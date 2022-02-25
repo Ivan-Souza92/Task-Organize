1eeed50a9ae5f7839a13a058d59cb244708b892c
Ivan-Souza92 <ivan.souza92@hotmail.com>
2022-02-25 18:47:08 -0300

    Criando DashController

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import App from './app';
 import TaskController from './controllers/TaskController';
 import UserController from './controllers/UserController';
+import DashController from './controllers/DashController';
 
 const app = new App([
   new UserController(),
-  new TaskController()
+  new TaskController(),
+  new DashController(),
 ]);
 
 app.listen(3333);
